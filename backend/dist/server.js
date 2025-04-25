3281e2366277bbe4fd60c5e1f3ccedbe837fb835
“Djisu” <pfleischer2002@yahoo.co.uk>
2025-04-25 11:21:51 +0000

    Removed frontendNew folder and added a frontend yml file

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -156,7 +156,7 @@ app.use('/api/textMessaging', textMessagingRoute);
 console.log('import.meta.url: ', import.meta.url);
 console.log('dirname(import.meta.url): ', dirname(import.meta.url));
 // Serve static files from the React app
-const frontendPath = path.resolve(dirname(fileURLToPath(import.meta.url)), '../../frontendNew/frontend/dist');
+const frontendPath = path.resolve(dirname(fileURLToPath(import.meta.url)), '../../frontend/dist');
 // Default route
 app.get('/', (req, res) => {
     res.sendFile(path.join(frontendPath, 'index.html'));
