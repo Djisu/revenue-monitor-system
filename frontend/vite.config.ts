6a4353be12a838d61479c436f4220c2c1f114221
“Djisu” <pfleischer2002@yahoo.co.uk>
2025-04-27 11:54:41 +0000

    Solving tscongif issue

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -19,12 +19,14 @@ export default defineConfig({
     chunkSizeWarningLimit: 1500, // Set the limit to 1.5 MB (1500 kB)
     outDir: './dist', // Ensure the output directory is set to 'dist'
     sourcemap: true, // optional
+    target: 'esnext',
   },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
   },
+  base: '', // Make sure it's set to an empty string if you're deploying to the root
   define: {
     'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
   },
@@ -34,10 +36,3 @@ export default defineConfig({
 
 
 
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
