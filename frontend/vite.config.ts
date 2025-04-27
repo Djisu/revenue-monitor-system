6535a30a5739e95a886af59cb19c198a68de680a
“Djisu” <pfleischer2002@yahoo.co.uk>
2025-04-27 18:43:47 +0000

    Correcting package.json

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  root: './src',
+  root: '.',
   plugins: [react()],
   server: {
     open: true, // Automatically open the browser
