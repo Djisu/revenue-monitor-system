2949ad57f167b28f680131b7aa80c86346fc30d7
“Djisu” <pfleischer2002@yahoo.co.uk>
2025-05-07 20:00:54 +0000

    Corrected puppeteer and express installation

--- backend/src/utils/puppeteerHelper.ts
+++ backend/src/utils/puppeteerHelper.ts
@@ -1,6 +1,7 @@
 // puppeteerHelper.ts
 import puppeteer from 'puppeteer';
 
+
 export async function getBrowser() {
   return await puppeteer.launch({
     headless: true, // make sure headless mode is enabled
