f4b215f5dd3db8172abd36b18f0371c22d975165
“Djisu” <pfleischer2002@yahoo.co.uk>
2025-05-08 08:44:21 +0000

    Added chrome executable path

--- backend/src/utils/puppeteerHelper.ts
+++ backend/src/utils/puppeteerHelper.ts
@@ -1,11 +1,18 @@
-// puppeteerHelper.ts
 import puppeteer from 'puppeteer';
 
-
 export async function getBrowser() {
+  const executablePath =
+    process.env.NODE_ENV === 'production'
+      ? '/opt/render/.cache/puppeteer/chrome/linux-136.0.7103.49/chrome-linux64/chrome'
+      : undefined;
+
+  console.log('Puppeteer executablePath:', executablePath || 'default');
+
   return await puppeteer.launch({
-    headless: true, 
-    args: ['--no-sandbox', '--disable-setuid-sandbox'], 
+    headless: true,
+    executablePath,
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
   });
 }
+
 
