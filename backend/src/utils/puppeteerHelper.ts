0f754ab8dd11ab6bf82b3a41e14269e75e8c33fb
“Djisu” <pfleischer2002@yahoo.co.uk>
2025-05-08 08:01:44 +0000

    Preparing chrome install for render.com deployment

--- backend/src/utils/puppeteerHelper.ts
+++ backend/src/utils/puppeteerHelper.ts
@@ -4,8 +4,8 @@ import puppeteer from 'puppeteer';
 
 export async function getBrowser() {
   return await puppeteer.launch({
-    headless: true, // make sure headless mode is enabled
-    args: ['--no-sandbox', '--disable-setuid-sandbox'], // important for Linux environments like Render
+    headless: true, 
+    args: ['--no-sandbox', '--disable-setuid-sandbox'], 
   });
 }
 
