478a221403832c3595c80dd0262253001e21e00a
“Djisu” <pfleischer2002@yahoo.co.uk>
2025-06-07 20:57:35 +0000

    Corrected the business operating permit document layout on the pdf

--- frontend/src/components/layout/NotFound.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-var NotFound = function () {
-    return (_jsx("div", { children: _jsx("h2", { children: "Error 404: Not found" }) }));
-};
-export default NotFound;
